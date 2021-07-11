57839f13cc6b5441c1336391fd7e899b0f17683b
AnnaKhristanova <annvahrameeva@mai.ru>
2021-07-12 02:32:16 +0300

    По заданию функция

--- sandbox/src/main/java/MyFirstProject.java
+++ sandbox/src/main/java/MyFirstProject.java
@@ -1,24 +1,22 @@
 package ru.stqa.pft.sandbox;
 
+import java.awt.*;
+
 import static java.lang.Math.*;
 
 public class MyFirstProject {
 
     public static void main(String[] args) {
 
-        double x1 = 0;
-        double x2 = 5;
-        double y1 = 0;
-        double y2 = 0;
-        System.out.println(distance(x1, x2, y1, y2));
+        Point p1 = new Point(0,6);
+        Point p2 = new Point(0,0);
+
+        System.out.println(distance(p1,p2));
     }
 
-    public static double distance(
-            double x1,
-            double y1,
-            double x2,
-            double y2)
+    public static double distance(Point p1, Point p2)
+
     {
-        return sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+        return sqrt((p2.y - p1.y) * (p2.y - p1.y) + (p2.x - p1.x) * (p2.x - p1.x));
     }
 }
---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,11 @@
+public class Point {
+
+public static double x;
+public static double y;
+
+public Point (double x, double y){
+    this.x=x;
+    this.y=y;
+}
+
+}
