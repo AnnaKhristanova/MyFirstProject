b15210925517b357f484570f9da9330d3b06c8a0
AnnaKhristanova <annvahrameeva@mai.ru>
2021-07-12 01:09:51 +0300

    Лекция 1 Работа функции

--- sandbox/src/main/java/MyFirstProject.java
+++ sandbox/src/main/java/MyFirstProject.java
@@ -1,8 +1,24 @@
 package ru.stqa.pft.sandbox;
 
+import static java.lang.Math.*;
+
 public class MyFirstProject {
 
-    public static void main(String[] arg) {
-        System.out.println("Hello,world!");
+    public static void main(String[] args) {
+
+        double x1 = 0;
+        double x2 = 5;
+        double y1 = 0;
+        double y2 = 0;
+        System.out.println(distance(x1, x2, y1, y2));
+    }
+
+    public static double distance(
+            double x1,
+            double y1,
+            double x2,
+            double y2)
+    {
+        return sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
     }
 }
