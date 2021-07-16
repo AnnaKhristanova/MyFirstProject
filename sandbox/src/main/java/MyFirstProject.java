9fceb6b5ed23df592fb5c76614764754ea0bb2a3
AnnaKhristanova <annvahrameeva@mai.ru>
2021-07-16 15:16:19 +0300

    Задание 2 Решение

--- sandbox/src/main/java/MyFirstProject.java
+++ sandbox/src/main/java/MyFirstProject.java
@@ -4,9 +4,10 @@ public class MyFirstProject {
 
     public static void main(String[] args) {
 
-        Point p1 = new Point(0,6);
-        Point p2 = new Point(0,0);
-
+        Point p1 = new Point(18,8.0);
+        Point p2 = new Point(5,10.0);
+        System.out.println("p1 is (" + p1.x + ", " + p1.y + ")");
+        System.out.println("p2 is (" + p2.x + ", " + p2.y + ")");
         System.out.println(p1.distance(p2));
     }
 
--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -2,13 +2,13 @@ import static java.lang.Math.sqrt;
 
 public class Point {
 
-public static double x;
-public static double y;
+public double x;
+public double y;
 
-    public Point (double x, double y){
-    this.x=x;
-    this.y=y;
-}
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
 
     public double distance(Point p2)
 
