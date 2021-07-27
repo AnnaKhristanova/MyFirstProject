282f0c0592a409dcb3f18ca1271d62d5c3383c64
AnnaKhristanova <annvahrameeva@mai.ru>
2021-07-27 17:13:13 +0300

    Добавлены проверки во все тесты

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -8,6 +8,9 @@ public class ContactModificationTests extends TestBase{
     @Test
     public void testGroupModification() {
         app.getNavigationHelper().goToHomePage();
+        if (!app.getContactHelper().isThereGroup()) {
+            app.getContactHelper().createContact(new UserData("Anna", "Khristanova", "Petrozavodsk", "888"));
+        }
         app.getContactHelper().initContactModification();
         app.getContactHelper().fillContactForm(new UserData("Anna1", "Khristanova1", "Petrozavodsk1", "999"));
         app.getContactHelper().submitContactModification();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -8,9 +8,12 @@ public class GroupModificationTests extends TestBase{
  @Test
     public void testGroupModification() {
      app.getNavigationHelper().goToGroupPage();
+  if (!app.getGroupHelper().isThereGroup()) {
+   app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+  }
      app.getGroupHelper().selectGroup();
      app.getGroupHelper().initGroupModification();
-     app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+     app.getGroupHelper().fillGroupForm(new GroupData("test1.1", "test1.2", "test1.3"));
      app.getGroupHelper().submitGroupModification();
      app.getGroupHelper().returnGroupPage();
  }
