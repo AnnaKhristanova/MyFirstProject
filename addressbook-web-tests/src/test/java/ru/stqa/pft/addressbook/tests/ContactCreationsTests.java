fbf86480d36ce48daaf00fb80aedac65e433142a
AnnaKhristanova <annvahrameeva@mai.ru>
2021-08-24 22:07:16 +0300

    Задание №13: Параметризация тестов для создания групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationsTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationsTests.java
@@ -1,23 +1,46 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.*;
+import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.UserData;
 import ru.stqa.pft.addressbook.model.Users;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.security.AnyTypePermission;
+import org.testng.annotations.DataProvider;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationsTests extends TestBase{
 
-  @Test
-  public void UserCreationTests() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validUsers() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/users.xml"));
+    String xml = "";
+    String line = reader.readLine();
+    while (line != null){
+      xml += line;
+      line = reader.readLine();
+    }
+    XStream xStream = new XStream();
+    xStream.addPermission(AnyTypePermission.ANY);
+    xStream.processAnnotations(UserData.class);
+    List<UserData> users =  (List<UserData>) xStream.fromXML(xml);
+    return users.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "validUsers")
+  public void UserCreationTests(UserData user) throws Exception {
     app.contact().goToHomePage();
     Users before = app.contact().all();
-    File photo = new File("src/test/resources/image_2021_05_30T15_13_35_761Z.png");
-    UserData user = new UserData()
-            .withFirstname("Anna2000").withLastname("Khristanova2000").withAddress("Petrozavodsk").withHomePhone("888").withPhoto(photo);
     app.contact().create(user);
     assertThat(app.contact().count(), equalTo(before.size() + 1));
     Users after = app.contact().all();
