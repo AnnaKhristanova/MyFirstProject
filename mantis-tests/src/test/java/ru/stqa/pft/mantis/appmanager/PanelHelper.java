1e917ad21340ad72348c5f96f1de09534cbe2d7e
AnnaKhristanova <annvahrameeva@mai.ru>
2021-09-11 11:50:02 +0300

    Задание №18: Реализовать тест для смены пароля в MantisBT

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/PanelHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/PanelHelper.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.mantis.appmanager;
 
+
+import org.hibernate.SessionBuilder;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
@@ -7,42 +10,47 @@ import java.util.List;
 
 public class PanelHelper extends HelperBase {
 
+
+    private SessionBuilder<SessionBuilder> sessionFactory;
+
     public PanelHelper(ApplicationManager app) {
         super(app);
     }
 
     public void login(String username, String password) {
         type(By.xpath("//input[@name='username']"), username);
+        click(By.xpath("//input[@class='width-40 pull-right btn btn-success btn-inverse bigger-110']"));
         type(By.xpath("//input[@name='password']"), password);
-        click(By.xpath("//input[@class='button']"));
+        click(By.xpath("//input[@class='width-40 pull-right btn btn-success btn-inverse bigger-110']"));
     }
 
     public void getUser() {
-
-        wd.findElement(By.xpath("//a[contains(text(),'Manage Users')]")).click();
-        List<WebElement> usersTable = wd.findElements(By.cssSelector("table.width100:nth-child(8) tbody:nth-child(1) tr.row-2"));
-        List<WebElement> cellsOfUsername = wd.findElements(By.cssSelector("tr.row-1 > td > a"));
-        for (int i = 0; i < usersTable.size(); i++) {
-            String userText = usersTable.get(i).getText();
-            if (userText.contains("reporter")) {
-                cellsOfUsername.get(i).click();
+        click(By.xpath("//i[@class='fa fa-gears menu-icon']"));
+        wd.findElement(By.cssSelector(String.format("a[href='/mantisbt-2.25.2/manage_user_page.php']"))).click();
+        List<WebElement> users = wd.findElements(By.cssSelector(".widget-main.no-padding .table-bordered td:first-child"));
+        List<WebElement> cells = wd.findElements(By.cssSelector("tr > td:nth-child(4)"));
+        for (WebElement cell : cells){
+            String status = cell.getText();
+            if (status.contains("reporter")) {
+                wd.findElement(By.cssSelector(String.format("tr > td > a"))).click();
                 return;
             }
         }
     }
 
     public String getEmail() {
+
         String email = wd.findElement(By.xpath("//input[@name='email']")).getAttribute("value");
         return email;
     }
 
-    public String getUsername() {
-        String username = wd.findElement(By.cssSelector("form:nth-child(1) table.width75:nth-child(2) tbody:nth-child(1) tr.row-1:nth-child(2) > td:nth-child(2)")).getText();
-        return username;
+    public String getRealname() {
+        String realname = wd.findElement(By.xpath("//input[@name='realname']")).getAttribute("value");
+         return realname;
     }
 
     public void resetPassword() {
-        wd.findElement(By.cssSelector("form:nth-child(1) > input.button:nth-child(3)")).click();
+        click(By.xpath("//input[@value='Reset Password']"));
     }
 
 
@@ -50,14 +58,16 @@ public class PanelHelper extends HelperBase {
         wd.get(confirmationLink);
     }
 
-    public void changePassword(String passworOld, String passwordNew) {
-        type(By.xpath("//input[@name='password']"), passworOld);
-        type(By.xpath("//input[@name='password_confirm']"), passwordNew);
-        click(By.xpath("//input[@class='button']"));
+    public void changePassword(String realname, String password, String password_confirm) {
+        type(By.xpath("//input[@name='realname']"), realname);
+        type(By.xpath("//input[@name='password']"), password);
+        type(By.xpath("//input[@name='password_confirm']"), password_confirm);
+        click(By.xpath("//button[@class='width-100 width-40 pull-right btn btn-success btn-inverse bigger-110']"));
     }
 
-    public WebElement logout() {
-        WebElement logout = wd.findElement(By.xpath("//a[contains(text(),'Logout')]"));
-        return logout;
+
+    public String getUsername() {
+        String username = wd.findElement(By.xpath("//input[@name='username']")).getAttribute("value");
+        return username;
     }
 }
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/ChangePasswordTest.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/ChangePasswordTest.java
@@ -4,15 +4,18 @@ import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.stqa.pft.mantis.appmanager.HttpSession;
 import ru.stqa.pft.mantis.model.MailMessage;
 import ru.lanwen.verbalregex.VerbalExpression;
 import ru.stqa.pft.mantis.tests.TestBase;
 
+import java.io.IOException;
 import java.util.List;
 
 import static org.testng.Assert.assertTrue;
 
 public class ChangePasswordTest extends TestBase {
+
 
     @BeforeMethod(alwaysRun = true)
     public void startMailServer() {
@@ -20,31 +23,33 @@ public class ChangePasswordTest extends TestBase {
     }
 
     @Test
-    public void changePassword() {
+
+    public void changePassword() throws IOException {
         app.panel().login("administrator", "root");
         app.panel().getUser();
         String email = app.panel().getEmail();
+        String realname = app.panel().getRealname();
+        String username = app.panel().getUsername();
         app.panel().resetPassword();
         List<MailMessage> mailMessages = app.mail().waitForMail(1, 100000);
         String confirmationLink = findConfirmationLink(mailMessages, email);
         app.panel().finish(confirmationLink);
-        String username = app.panel().getUsername();
-        app.panel().changePassword("12345678", "12345678");
-        app.panel().login(username, "12345678");
-        WebElement logoutButton = app.panel().logout();
-        assertTrue(logoutButton.isDisplayed());
-
+        app.panel().changePassword(realname, "passwordNew", "passwordNew");
+        HttpSession session = app.newSession();
+        session.login(username, "passwordNew");
+        assertTrue(session.isLoggedInAs(username));
+        app.panel().login(username, "passwordNew");
     }
 
     private String  findConfirmationLink(List<MailMessage> mailMessages, String email) {
-        // znajdujemy tylko ten który przychopdzi na konkretny adres
+
         MailMessage mailMessage = mailMessages.stream().filter((m) -> m.to.equals(email)).findFirst().get();
-        // Liczba symboli która nie jest spacja nonSpace jeden lub wiecej oneOrMore
+
         VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
         return regex.getText(mailMessage.text);
     }
 
-    //Stop serwera
+
     @AfterMethod(alwaysRun = true)
     public void stopMailServer() {
         app.mail().stop();
